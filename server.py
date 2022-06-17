b6bcf8417f728389f4bb864b4c463c2371feb116
Jeff Harms <jeffdharms@gmail.com>
2022-06-17 12:01:47 -0700

    Bug Fixing

--- server.py
+++ server.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "password"
 
+
 @app.route('/')
-@app.route('/Home')
+@app.route('/survey')
 def display_form():
     return render_template("survey.html")
 
-@app.route('/submit', methods=['POST'])
+
+@app.route('/survey', methods=['POST'])
 def create_user():
-    print("Got Post Info")
-    # print(request.form)
-    # name = request.form['name']
-    # email = request.form['email']
-    return redirect("/result")	 
-    
+    print(request.form)
+    if (display_results == None):
+        return redirect('/survey')
+    else:
+        session['name'] = request.form['name']
+        session['location'] = request.form['location']
+        session['language'] = request.form['language']
+        session['comments'] = request.form['comments']
+        return redirect("/result")	 
+
+
 @app.route('/result')
-def show_ninja(your_name, dojo_location, favorite_language, comments):
-    print("Showing the User Info From the Form")
-    # print(request.form)
-    return render_template("survey_result.html", your_name = your_name, dojo_location = dojo_location, favorite_language = favorite_language, comments = comments)
+def display_results():
+    return render_template("survey_result.html")
+
 
 if (__name__) == "__main__":
     app.run(debug=True)
